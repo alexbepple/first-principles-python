26e2c8bd9a4224ecd8f85791574e1564054f7523
Alex Bepple <alex@bepple.de>
2013-05-22 03:19:19 +0200

    + improve book tests

--- test/unit/book_test.py
+++ test/unit/book_test.py
@@ -1,14 +1,28 @@
 from nose.tools import *
 
 class Book_Test:
+
     @istest
-    def can_be_lent_and_returned(self):
+    def can_be_lent_by_default(self):
         book = Book()
         assert_true(book.islendable())
-        book.borrow()
-        assert_false(book.islendable())
-        book.give_back()
-        assert_true(book.islendable())
+
+
+class BookOnceBorrowed_Test:
+
+    def setUp(self):
+        self.book = Book()
+        self.book.borrow()
+
+    @istest
+    def is_not_lendable(self):
+        assert_false(self.book.islendable())
+
+    @istest
+    def is_lendable_again_after_given_back(self):
+        self.book.give_back()
+        assert_true(self.book.islendable())
+
 
 class Book:
     def __init__(self):
