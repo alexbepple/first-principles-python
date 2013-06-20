654b93710937c965c3e8f3d46c7555e5d58409df
Alex Bepple <alex@bepple.de>
2013-06-20 11:58:02 +0200

    + improve readability for StringCreator_Test

--- test/unit/string_creator_test.py
+++ test/unit/string_creator_test.py
@@ -1,12 +1,32 @@
 from nose.tools import *
 import re
+from hamcrest import *
 
 class StringCreator_Test:
     @istest
     def createsOnlyWhitespace(self):
-        result = StringCreator().create()
-        cleaned_up = re.sub(r'\s', '', result)
-        ok_(not cleaned_up)
+        assert_that(StringCreator().create(), contains_only_whitespace())
+
+
+
+from hamcrest.core.base_matcher import BaseMatcher
+class StringMatchesPattern(BaseMatcher):
+
+    def __init__(self, pattern):
+        self.pattern = pattern
+
+    def describe_to(self, description):
+        description.append_text("a string matching '") \
+                .append_text(self.pattern) \
+                .append_text("'")
+
+    def _matches(self, item):
+        return re.match(self.pattern, item)
+
+def contains_only_whitespace():
+    return StringMatchesPattern(r'^\s*$')
+
+
 
 class StringCreator:
     def create(self):
