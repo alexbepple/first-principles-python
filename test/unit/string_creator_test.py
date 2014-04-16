c788bbe8b8ff7638b5ac25116d22068e1c3ee5fb
Sebastian Sanitz <sebastian@sanitz.net>
2014-04-16 11:55:54 +0200

    Formatierung nach pep8

--- test/unit/string_creator_test.py
+++ test/unit/string_creator_test.py
@@ -1,11 +1,13 @@
 import re
-from hamcrest import *
+
+from hamcrest.core.base_matcher import BaseMatcher
+from hamcrest import assert_that
+
 
 def test_creates_only_whitespace():
     assert_that(StringCreator().create(), contains_only_whitespace())
 
 
-from hamcrest.core.base_matcher import BaseMatcher
 class StringMatchesPattern(BaseMatcher):
 
     def __init__(self, pattern):
@@ -13,15 +15,15 @@ class StringMatchesPattern(BaseMatcher):
 
     def describe_to(self, description):
         description.append_text("a string matching '") \
-                .append_text(self.pattern) \
-                .append_text("'")
+            .append_text(self.pattern) \
+            .append_text("'")
 
     def _matches(self, item):
         return re.match(self.pattern, item)
 
+
 def contains_only_whitespace():
     return StringMatchesPattern(r'^\s*$')
-
 
 
 class StringCreator:
